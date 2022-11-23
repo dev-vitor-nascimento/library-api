c28443d29ee3e34a855a4d80a1b37e89e81f9789
altislab.joao <vitor.nascimento@altislab.com.br>
2022-11-23 13:51:28 -0300

    Revert "add publishers prisma model"
    This reverts commit 1b64bfd2bc00fe7948bc38bc741e47487d9d636d.

---
+++ src/infrastructure/database/prisma/migrations/20221117145626_create_users/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Customer` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `city` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Customer_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
