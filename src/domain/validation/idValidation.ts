ba5611a1739af1e6db51246edb0ba0380e8a00f0
altislab.joao <vitor.nascimento@altislab.com.br>
2022-11-29 22:03:35 -0300

    Removed express-validator

--- src/domain/validation/idValidation.ts
+++
@@ -1,9 +0,0 @@
-import { param } from "express-validator"
-
-const idValidation = [
-    param("id")
-        .notEmpty().withMessage("Id is required")
-        .isUUID().withMessage("Id must be of type UUID")
-]
-
-export { idValidation }
--- src/ports/http/routes/customers.routes.ts
+++ src/ports/http/routes/customers.routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import { customerValidation } from "@domain/validation/CustomerValidation";
-import { idValidation } from "@domain/validation/idValidation";
 import { CreateCustomerController } from "../controllers/customers/CreateCustomerController";
 import { DeleteCustomerController } from "../controllers/customers/DeleteCustomerController";
 import { FindAllCustomersController } from "../controllers/customers/FindAllCustomersController";
 import { FindCustomerByIdController } from "../controllers/customers/FindCustomerByIdController";
 import { UpdateCustomerController } from "../controllers/customers/UpdateCustomerController";
-import { validationHandler } from "../middlewares/validationHandler";
 
 const customersRoutes = Router();
 
@@ -16,10 +13,10 @@ const deleteCustomerController = new DeleteCustomerController();
 const findCustomerByIdController = new FindCustomerByIdController();
 const findAllCustomersController = new FindAllCustomersController();
 
-customersRoutes.post("/", validationHandler(customerValidation), createCustomerController.handle);
-customersRoutes.put("/:id", validationHandler([...idValidation, ...customerValidation]), updateCustomerController.handle);
-customersRoutes.delete("/:id", validationHandler(idValidation), deleteCustomerController.handle);
-customersRoutes.get("/:id", validationHandler(idValidation), findCustomerByIdController.handle);
+customersRoutes.post("/", createCustomerController.handle);
+customersRoutes.put("/:id", updateCustomerController.handle);
+customersRoutes.delete("/:id", deleteCustomerController.handle);
+customersRoutes.get("/:id", findCustomerByIdController.handle);
 customersRoutes.get("/", findAllCustomersController.handle);
 
 export { customersRoutes }
